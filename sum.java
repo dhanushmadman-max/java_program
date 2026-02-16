602b0aa09eb810b63ec99802db622e811d471363
dhanushmadman-max <dhanushmadman@gmail.com>
2026-02-16 09:52:11 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+  class sum
+    {
+      public static void main(static args[])
+      {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("sum of two numbers"+sum);
+        {
+    }
